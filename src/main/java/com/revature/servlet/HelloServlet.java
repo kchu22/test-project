d1ccd0502733dc904fe9d9d67e2bbc9764a10ea6
Kerwin Chu <theguygalaxy@gmail.com>
2019-08-21 10:51:41 -0400

    Testing Jenkins CI

--- src/main/java/com/revature/servlet/HelloServlet.java
+++ src/main/java/com/revature/servlet/HelloServlet.java
@@ -10,6 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().write("Hello this is from test-project.");
+		resp.getWriter().write("Hello this is from test-project. Something");
 	}
 }
